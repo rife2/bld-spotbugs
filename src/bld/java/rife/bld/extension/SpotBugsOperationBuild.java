1a1fe73b5a1c27b172d903a9984ed241b95e9b6d
Erik C. Thauvin <erik@thauvin.net>
2026-04-30 01:38:55 -0700

    Version 1.1.0

--- src/bld/java/rife/bld/extension/SpotBugsOperationBuild.java
+++ src/bld/java/rife/bld/extension/SpotBugsOperationBuild.java
@@ -35,7 +35,7 @@ public class SpotBugsOperationBuild extends Project {
         pkg = "rife.bld.extension";
         name = "SpotBugs";
         archiveBaseName = "bld-spotbugs";
-        version = version(1, 0, 0);
+        version = version(1, 1, 0);
 
         javaRelease = 17;
 

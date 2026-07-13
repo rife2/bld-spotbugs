56053f8652278bbc432d05ef80d8989137139794
Erik C. Thauvin <erik@thauvin.net>
2026-07-12 19:27:42 -0700

    Bump Jackson Databind to version 2.22.1

--- src/bld/java/rife/bld/extension/SpotBugsOperationBuild.java
+++ src/bld/java/rife/bld/extension/SpotBugsOperationBuild.java
@@ -49,7 +49,7 @@ public class SpotBugsOperationBuild extends Project {
                 .include(dependency("com.uwyn.rife2", "bld-extensions-tools",
                         version(1, 3, 0, "SNAPSHOT")))
                 .include(dependency("com.fasterxml.jackson.core", "jackson-databind",
-                        version(2, 22, 0)))
+                        version(2, 22, 1)))
                 .include(dependency("com.uwyn.rife2", "bld",
                         version(2, 3, 1, "SNAPSHOT")));
         scope(provided)

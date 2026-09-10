4686aaa551f6a01e584aee40f3d6ad2a56eb083b
Erik C. Thauvin <erik@thauvin.net>
2026-09-09 20:16:31 -0700

    Bump Extensions Tools to version 1.3.0

--- src/bld/java/rife/bld/extension/SpotBugsOperationBuild.java
+++ src/bld/java/rife/bld/extension/SpotBugsOperationBuild.java
@@ -47,7 +47,7 @@ public class SpotBugsOperationBuild extends Project {
         var junit = version(6, 1, 3);
         scope(compile)
                 .include(dependency("com.uwyn.rife2", "bld-extensions-tools",
-                        version(1, 3, 0, "SNAPSHOT")))
+                        version(1, 3, 0)))
                 .include(dependency("com.fasterxml.jackson.core", "jackson-databind",
                         version(2, 22, 2)))
                 .include(dependency("com.uwyn.rife2", "bld",
